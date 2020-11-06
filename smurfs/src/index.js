cbc91b4fbafe782946818a3812083b6f737c30c7
unknown <xiongjoh@gmail.com>
2020-11-06 10:20:30 -0600

    install dependencies and create store/reducer

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+
+import reducer from './store/reducers'
+
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import "./index.css";
+
+const store = createStore(reducer, applyMiddleware(thunk))
+
+console.log(store.getState())
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
---
+++ smurfs/src/store/reducers/index.js
@@ -0,0 +1,15 @@
+
+const initialState = {
+    isLoading: false;
+    smurfs:[],
+    error:'',
+}
+
+const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        default:
+            return state
+    }
+}
+
+export default reducer
